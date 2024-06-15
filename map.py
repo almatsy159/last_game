46b717d368dca20de23395cee5a2b55f3ef9b6c6
almatsy159 <drago.gl159@gmail.com>
2024-06-15 03:23:14 +0200

    adjusted archi

--- map.py
+++ map.py
@@ -1,5 +1,7 @@
 import pygame as pg
 
+
+
 """
 g = pg.init()
 screen = pg.display.set_mode((0,0))
@@ -36,82 +38,80 @@ class Panel:
     def draw(self,screen,pos=(150,150)):
         self.blit(screen,pos)
 """
-class Point:
+class Point(pg.sprite.Sprite):
+    points = pg.sprite.Group()
     def __init__(self,x=0,y=0,z=0,color="black"):
+        super().__init__()
         self.x =  x
         self.y = y
         self.z = z
         self.pos = (self.x,self.y,self.z)
-        self.color = color 
+        self.color = color
+        self.add(Point.points)
     
     def __str__(self):
         return f"{self.x},{self.y},{self.z}"
     
-    def __add__(self,point):
-        x = self.x + point.x
-        y = self.y + point.y
-        z = self.z + point.z
-        return Point(x,y,z)
+    #def __add__(self,point):
+    #    x = self.x + point.x
+    #    y = self.y + point.y
+    #    z = self.z + point.z
+    #    return Point(x,y,z)
     
-    def draw(self,screen):
-        pg.draw.circle(screen,self.color,(self.x,self.y))
+    #def draw(self,screen):
+    #    pg.draw.circle(screen,self.color,(self.x,self.y))
         
-class Frame:
-    def __init__(self,beg,end,color="white",grid=False):
+class Frame(pg.sprite.Sprite):
+    frames = pg.sprite.Group()
+    def __init__(self,beg,h,w,color="white",grid=False):
+        super().__init__()
         self.color = color
-        #print(beg,end)
+        self.w = w
+        self.h = h
         self.beg = beg
-        self.end = end
-        self.w = self.end.x - self.beg.x
-        self.h = self.end.y - self.beg.y
+        self.center = (self.beg.x+self.w//2,self.beg.y+self.h//2)
+
         print(self.w,self.h)
-        self.rect = pg.Rect(self.beg.x,self.beg.y,self.w,self.h)
-        self.surface = pg.Surface((self.w,self.h))
-        self.elements = {}
-        self.grid = grid
         
-    def draw(self,screen):
-        pg.draw.rect(screen,self.color,self.rect)
+        self.image= pg.Surface((self.w,self.h))
+        self.image.fill(self.color)
+        self.rect = self.image.get_rect()
+        self.rect.center=self.center
+        self.elements = pg.sprite.Group()
+        self.grid = grid
+        self.add(Frame.frames)
+        
+    #def draw(self,screen):
+        #pg.draw.rect(screen,self.color,self.rect)
         #if self.grid == True:
         #    self.make_grid(screen)
-        for i,j in self.elements :
-            j.draw(screen)
-            
-    def make_grid(self,screen,block=25,color="green"):
-        x_init = self.beg.x
-        y_init = self.beg.y
-        x_end = self.end.x
-        y_end = self.end.y
-        #print(x_init,y_init)
-        #print(x_end,y_end)
-        
-        for x in range(x_init,x_end,block):
-            for y in range(y_init,y_end,block):
-                rect = pg.Rect(x,y,block,block)
-                pg.draw.rect(screen,color,rect,1)
-    
+        #for i,j in self.elements :
+        #    j.draw(screen)
     def clicked(self):
         print("clicked on frame")
-
-
         
-class Tile:
+        
+class Tile(pg.sprite.Sprite):
+    tiles = pg.sprite.Group()
     x = 50
     y = 50
     def __init__(self,name,cg,color="green"):
+        super().__init__()
         self.name = name
         self.color = color
         self.dx = Tile.x
         self.dy = Tile.y
-        self.img= pg.Surface((self.dx,self.dy))
+        self.image = pg.Surface((self.dx,self.dy))
+        self.image.fill(self.color)
         self.cg = cg
-        self.rect = self.img.get_rect()
+        self.rect = self.image.get_rect()
         self.rect.center = (self.cg.x,self.cg.y)
         self.id_item = 0
-        self.contain = {}
+        self.elements = {}
+        self.add(Tile.tiles)
     
-    def draw(self,screen):
-        pg.draw.rect(screen,self.color,self.rect)
+    #def draw(self,screen):
+    #    pg.draw.rect(screen,self.color,self.rect)
         
     def move(self,x=0,y=0,z=0):
         add = Point(x,y,z)
@@ -121,7 +121,7 @@ class Tile:
     def add_item(self,item,id=None):
         if id ==None:
             id = self.id_item
-        self.contain[id] = item
+        self.elements[id] = item
         return True
         
     def clicked(self):
@@ -130,17 +130,39 @@ class Tile:
         else :
             self.color = "green"
         return self.name
+    
+    #def update(self,game):
+    #    if self.cg.x > game.w:
+            
         
 class Game:
-    def __init__(self,screen):
+    def __init__(self,screen,ytile=10,xtile=10,font="Arial",font_size=30):
+        self.ytile = ytile
+        self.xtile = xtile
+        self.font = pg.font.SysFont(font,font_size)
+        self.bg = pg.image.load("/home/alma/Documents/prog/asset/bg0.jpg")
+        self.bg = self.bg.convert()
+        self.bg.set_alpha(200)
+        self.status = "creation"
         self.screen = screen
+        self.screen.blit(self.bg,(0,0))
         self.running = True
         self.clock = pg.time.Clock()
-        self.elements = {}
+        self.elements = {"frames":Frame,"tiles":Tile}
         self.new = None
+        self.selected = None
         self.w,self.h = self.screen.get_size()
+        print(self.w,self.h)
         self.center = int(self.w/2),int(self.h/2)
         self.last_event = None
+    
+    def draw_text(self,text="",color="grey",x=0,y=0):
+        img = self.font.render(text,True,color)
+        self.screen.blit(img,(x,y))
+    
+    def mouse_click(self,event):
+        self.draw_text(f"{event.pos}","white",event.pos[0]+100,event.pos[1]+100)
+            
 
     def handling_event(self):
         event = None
@@ -148,19 +170,26 @@ class Game:
             if event.type == pg.QUIT :
                 self.running = False
             if event.type == pg.KEYDOWN:
-                if event.key == pg.k_q:
+                if event.key == pg.K_q:
                     self.running = False
             if event.type == pg.MOUSEBUTTONDOWN:
                 print(event)
-                for k,e in self.elements.items():
-                    if e.rect.collidepoint(event.pos):
-                        print("collided!")
-                        e.clicked()
-                    else:
-                        print("nothing clicked on !")
+                if self.status == "creation":
+                    click = Point(event.pos[0],event.pos[1])
+                    #tile = Tile("noname",click,"blue")
+                    self.mouse_click(event)
+                
+                #for k,e in self.elements.items():
+                #    pass
+                    #if e.rect.collidepoint(event.pos):
+                    #    print("collided!")
+                    #    e.clicked()
+                    #else:
+                    #    print("nothing clicked on !")
                         
     def update(self):
-        pass
+        Frame.frames.update()
+        Tile.tiles.update()
     
     def add_element(self,name,element):
         self.elements[name] = element
@@ -168,23 +197,70 @@ class Game:
     def display(self):
         pg.display.flip()
     
-    def draw(self):
-        for i,j in self.elements.items():
-            #print(i,j)
-            j.draw(self.screen)
+    def draw(self,opt=None):
+        #for i,j in self.elements.items():
+        #    #print(i,j)
+        #    j.draw(self.screen)
+        Tile.tiles.draw(self.screen)
+        Frame.frames.draw(self.screen)
+        #self.draw_text("A","black",150,150)
+        #Point.points.draw()
+        #for t in Tile.tiles:
+            #print(t.cg.x)
+    
+        #self.draw_text(f"{t.cg.x},{t.cg.y}","grey",t.cg.x,t.cg.y)
         
     def first(self):
-        center = Point(self.center[0],self.center[1],0)
-        center_tile = Tile("center",center)
-        self.elements["center_tile"] = center_tile
+        #center = Point(self.center[0],self.center[1],0)
+        #center_tile = Tile("center",center)
+        #self.tiles = Tile.tiles
+        #center_tile.add(Tile.tiles)
+        #self.elements["center_tile"] = center_tile
         #panel = Panel("hello_world")
         #self.elements["panel"] = panel
-        test_beg = Point(50,50)
-        test_end = Point(150,150)
-        test_color = "red"
-        test_frame = Frame(test_beg,test_end,test_color,grid=True)
-        self.elements["test_frame"] = test_frame
-
+        #test_beg = Point(50,50)
+        #test_w
+        #test_h
+        #test_color = "red"
+        #test_frame = Frame(test_beg,test_end,test_color,grid=True)
+        
+        #dialog_beg = Point(1700,200)
+        #dialog_w = 100
+        #dialog_h = 150
+        #dialog = Frame(dialog_beg,dialog_w,dialog_h,"purple")
+        
+        #print(Tile.tiles)
+        #test_frame.add(Frame.frames)
+        #self.elements["test_frame"] = test_frame
+        
+        #t1 = self.draw_text("hello world",color="white",x=500,y=200)
+        x_init = 100
+        y_init = 100
+        bloc = 50
+        delta = 1
+        cpt=0
+        
+        for x in range(self.xtile): 
+            for y in range(self.ytile):
+                if cpt%2 ==0:
+                    color = "white"
+                    ncolor = "grey"
+                else:
+                    color = "grey"
+                    ncolor="black"
+                    
+                if x%2 ==1:
+                    tmp = color
+                    color = ncolor
+                    ncolor = tmp
+                name = f"{x},{y}"
+                xt = x_init + (x * bloc)
+                yt = y_init + (y * bloc)
+                cg = Point(xt,yt,0)
+                print(x,y)
+                tile = Tile(name,cg,color)
+                cpt += 1
+        print(cpt)
     def run(self):
         self.first()
         while self.running == True:
@@ -197,6 +273,7 @@ class Game:
 if __name__ == "__main__":
     pg.init()
     screen = pg.display.set_mode((0,0))
+    global g1
     g1 = Game(screen)
     g1.run()
     pg.quit()
